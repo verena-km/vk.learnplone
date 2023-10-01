18529460f17298ab02d9f2f590620e11825e782a
Verena Kunz-Meier <verena@kunz-meier.de>
2023-10-01 20:20:50 +0200

    request parameter example

--- src/vk/learnplone/views/request_parameter_view.py
+++ src/vk/learnplone/views/request_parameter_view.py
@@ -1,24 +1,16 @@
 # -*- coding: utf-8 -*-
 
-# from vk.learnplone import _
 from Products.Five.browser import BrowserView
 from zope.interface import implementer
 from zope.interface import Interface
 
-
-# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-
 class IRequestParameterView(Interface):
     """Marker Interface for IRequestParameterView"""
 
-
 @implementer(IRequestParameterView)
 class RequestParameterView(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('request_parameter_view.pt')
-
     def __call__(self):
-        # Implement your own actions:
         return self.index()
+
+
+
