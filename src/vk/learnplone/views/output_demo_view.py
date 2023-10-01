f50b77139b857ba26deec04bda5971bf5eabaa2b
Verena Kunz-Meier <verena@kunz-meier.de>
2023-10-01 08:16:12 +0200

    view output demo

--- src/vk/learnplone/views/output_demo_view.py
+++ src/vk/learnplone/views/output_demo_view.py
@@ -1,24 +1,19 @@
 # -*- coding: utf-8 -*-
 
-# from vk.learnplone import _
 from Products.Five.browser import BrowserView
 from zope.interface import implementer
 from zope.interface import Interface
 
-
-# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-
 class IOutputDemoView(Interface):
     """Marker Interface for IOutputDemoView"""
 
-
 @implementer(IOutputDemoView)
 class OutputDemoView(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('output_demo_view.pt')
 
     def __call__(self):
-        # Implement your own actions:
+        self.output_var = "This output comes from a variable."
+        print(self.request.items())
         return self.index()
+
+    def output_method(self):
+        return ("This output comes from a method")
