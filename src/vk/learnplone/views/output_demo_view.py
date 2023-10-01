88c9d067c45fb1bc729f2804e37cc64adf2fd24c
Verena Kunz-Meier <verena@kunz-meier.de>
2023-10-01 08:17:02 +0200

    "Add view: requestparameter"

--- src/vk/learnplone/views/output_demo_view.py
+++ src/vk/learnplone/views/output_demo_view.py
@@ -4,16 +4,17 @@ from Products.Five.browser import BrowserView
 from zope.interface import implementer
 from zope.interface import Interface
 
+
 class IOutputDemoView(Interface):
     """Marker Interface for IOutputDemoView"""
 
+
 @implementer(IOutputDemoView)
 class OutputDemoView(BrowserView):
-
     def __call__(self):
         self.output_var = "This output comes from a variable."
         print(self.request.items())
         return self.index()
 
     def output_method(self):
-        return ("This output comes from a method")
+        return "This output comes from a method"
